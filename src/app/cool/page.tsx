bd5e24aacd0746ab0a19a7a01f9840646c7c2e3e
Rick Sarkar <rickdsarkar@gmail.com>
2026-08-18 13:12:31 -0400

    finish cool/page about me + finalize gif adding

--- src/app/cool/page.tsx
+++ src/app/cool/page.tsx
@@ -1,4 +1,6 @@
-import dunkGif fro
+import dunkGif from '../assets/dunk.gif';
+import Image from 'next/image';
+
 export default function Kewl() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-primary text-white gap-5">
@@ -8,11 +10,12 @@ export default function Kewl() {
       Feel free to put links, images, whatever! 
       Don't worry about styling- we aren't grading you on this- it's just to get to know you better! :) 
       */}
-	<p>
+	<p style={{ marginInline: '10px' }}>
 	Hi! My name's Rick, and I'm an incoming fourth year CS major. 
-	One of the best things that's happened to me during my time at GT was joining CS2110 as a TA.
+	One of the best things that's happened to me during my time at GT was joining CS2110 as a TA. <br />
 	I've met a lot of great people and it made my time as a student feel special.
 	</p>
+	<Image src={dunkGif} alt="" />
     </div>
   );
 }
